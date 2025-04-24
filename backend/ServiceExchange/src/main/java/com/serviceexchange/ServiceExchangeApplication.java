05ccb492e513e55defc24fe261f286ba1fc9b740
DominikW <78363628+intorwacja@users.noreply.github.com>
2025-04-24 17:49:54 +0200

    Initial Commit

---
+++ backend/ServiceExchange/src/main/java/com/serviceexchange/ServiceExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.serviceexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceExchangeApplication.class, args);
+    }
+
+}
---
+++ backend/ServiceExchange/src/test/java/com/serviceexchange/ServiceExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
